407f381bfd82d67304935fd2e299cca33f5e66b3
David Macphail <aspiring.coder@gmail.com>
2011-09-09 08:19:07 -0400

    minor bugfix

--- ntlm-extractor.sh
+++ ntlm-extractor.sh
@@ -17,7 +17,7 @@
 # set up main function as seperate file to keep s_client output to a minimum
  
 cat > main.sh <<EOF
-#!/usr/bin/env/bash 
+#!/usr/bin/env/ bash 
 echo -e -n "GET / HTTP/1.0\r\nAuthorization: NTLM TlRMTVNTUAABAAAAB4IIogAAAAAAAAAAAAAAAAAAAAAFASgKAAAADw==\r\nConnection: close\r\n\r\n\r\n" | nc -w 10 \$1 \$2  | grep "WWW-Authenticate:" | tail -n1 | awk '{print \$3}' | base64 -d -i |  tr -cd '\001\002\003\005\006\016\017\005\004\40-\176' |tee ${1}.full | sed 's/\(^.*'\$(printf '\1')'\)\(.*\)\('\$(printf '\4')'.*$\)/\2/'  | tr -cd "[:alnum:].$" |tee ${1}.full.sanitised > rslt
 #(echo -e -n "GET / HTTP/1.0\r\nAuthorization: NTLM TlRMTVNTUAABAAAAB4IIogAAAAAAAAAAAAAAAAAAAAAFASgKAAAADw==\r\nConnection: close\r\n\r\n\r\n"; sleep 10) | openssl s_client -connect \$1:\$2 -quiet  | grep "WWW-Authenticate:" | tail -n1 | awk '{print \$3}' | base64 -d -i |  tr -cd '\001\002\006\016\005\40-\176' |tee ${1}.full | tr -cd "[:alnum:].$" |tee ${1}.full.sanitised| 's/\(^.*'\$(printf '\1')'\)\(.*\)\('\$(printf '\4')'.*$\)/\2/' > rslt
 sed -e 's/\(^.*'\$(printf '\17')'\)\(.*\)\('\$(printf '\2')'.*$\)/\2/' ${1}.full | tr -cd "[:alnum:][:punct:]"> ${1}.ADDomain
